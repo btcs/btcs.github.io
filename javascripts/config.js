3bf3280757fb696c9d905999159d59c0e94fb0e1
cyio <ibeceo@gmail.com>
2014-02-16 09:58:27 +0800

    up

--- javascripts/config.js
+++ javascripts/config.js
@@ -2,5 +2,5 @@ var disqus_shortname = '';
 var hostbase = 'http://btcs.github.io';
 var githubname = 'btcs';
 var repos = 'btcs.github.io';
-var sitetitle = 'iSay';
+var sitetitle = 'BTCs';
 var rss = '';
